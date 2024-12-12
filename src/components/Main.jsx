e96d2fc67065dc5f5e88ae76330e2e31510845fb
Seboo Dogar <seboodogar@gmail.com>
2024-12-12 20:29:11 +0500

    Main added

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+        <>
+        <section className="">
+            <div className="">
+                <div className="">
+                    <div className="">
+                        <a href="" className="classname" target="_blank"></a>
+                        <a href="" className="classname" target="_blank"></a>
+                        <a href="" className="classname" target="_blank"></a>
+                    </div>
+                    <div className="">
+                        <img src="" alt=""></img>
+                    </div>
+                </div>
+            </div>
+        </section>        
+        </>
+    );
+};
