215155f68019371865a161eab969590b64641661
Seboo Dogar <seboodogar@gmail.com>
2024-12-13 10:36:02 +0500

    Make some basic changes in Main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,17 @@ import { FaRegMoon } from "react-icons/fa";
 export default function Header() {
     return (
         <header className='py-5'>
-            <div className='container mx-auto flex justify-between items-center'>
+            <div className='container mx-auto flex justify-between items-center bg-slate-400'>
                 <h1 className='text-2xl font-bold'>SEBOO</h1>
                 <nav className=''>
-                    <ul className="flex space-x-6 font-semibold">
+                    <ul className="flex items-center space-x-6 font-semibold">
                         <li><a href="#home" className="hover:underline">Home</a></li>
                         <li><a href="#about" className="hover:underline">About</a></li>
                         <li><a href="#skills" className="hover:underline">Skills</a></li>
                         <li><a href="#services" className="hover:underline">Services</a></li>
                         <li><a href="#portfolio" className="hover:underline">Portfolio</a></li>
                         <li><a href="#contact" className="hover:underline">Contact</a></li>
-                        <li><a href="#nightMood" className="inline"><FaRegMoon /></a></li>
+                        <li><a href="#nightMood" className=""><FaRegMoon /></a></li>
                     </ul>
                 </nav>
             </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,24 @@
 import { FiLinkedin } from "react-icons/fi";
 import { TfiWorld } from "react-icons/tfi";
 import { FiGithub } from "react-icons/fi";
+import { FiDownload } from "react-icons/fi";
 
 export default function Main() {
     return (
         <>
         <section className="bg-gray-100 py-10">
-            <div className="container mx-auto px-4">
+            <div className="container mx-auto px-4 bg-slate-400">
                 <div className="flex items-center justify-center">
-                    <div className="mb-6 space-x-4">
+                    <div className="space-x-4">
                         <a href="https://www.linkedin.com/in/sohaib-aslam-dev/" className="" target="_blank" rel="noopener noreferrer"><FiLinkedin /></a>
                         <a href="#" className="" target="_blank" rel="noopener noreferrer"><TfiWorld /></a>
                         <a href="https://github.com/Seboo-Dogar" className="" target="_blank" rel="noopener noreferrer"><FiGithub /></a>
                     </div>
-                    <div className="">
-                        <h1>Hi, I'm Sohaib</h1>
+                    <div className="space-y-2">
+                        <h1>Hi, i am Sohaib</h1>
                         <h3>Frontend Developer</h3>
                         <p>High level experience in web design and development knowledge, producing quality work.</p>
-                        <a href="./src/assets/MyCV.pdf" download="MyCV.pdf" className="bg-blue-600 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 transition duration-300">Download CV</a>
+                        <a href="./src/assets/MyCV.pdf" download="MyCV.pdf" className="bg-[#6c55e1] text-white py-2 px-4 rounded-lg inline-flex items-center">Download CV <FiDownload className="ml-2" /></a>
                     </div>
                     <div className="">
                         <img src="./src/assets/Seboo.png" alt="Seboo Profile" className="w-40 h-40"/>
