11a841484b85948176baf406e18a9b8f6aedfede
Seboo Dogar <seboodogar@gmail.com>
2024-12-17 22:50:49 +0500

    Footer Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-        <footer className="py-6">
-            <div className="container mx-auto flex flex-col items-center justify-between space-y-4 text-center md:flex-row md:space-y-0 md:text-left">
+        <footer className="">
+            <div className="container mx-auto flex items-center justify-between text-center border-t p-6 border-gray-500">
                 <div className="flex space-x-4">
                     <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                         <FaFacebook className="text-xl hover:text-[#6c55e1]" />
@@ -18,12 +18,8 @@ export default function Footer() {
                         <FaGithub className="text-xl hover:text-[#6c55e1]" />
                     </a>
                 </div>
-
-                {/* Copyright */}
                 <div>
-                    <p className="text-sm">
-                        &copy; {new Date().getFullYear()} Sohaib Aslam. All rights reserved.
-                    </p>
+                    <p className="text-sm">&copy; {new Date().getFullYear()} Sohaib Aslam. All rights reserved.</p>
                 </div>
             </div>
         </footer>
