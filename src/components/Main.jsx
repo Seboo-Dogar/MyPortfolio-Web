dfda00b992bb4d7e84ab7e210039afa1eb119a4b
Seboo Dogar <seboodogar@gmail.com>
2025-01-02 00:28:01 +0500

    Home Page Done...Responsive

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,40 +6,88 @@ import { CgMouse } from "react-icons/cg";
 import { TiArrowDown } from "react-icons/ti";
 
 export default function Main() {
-
     const mangeScrolling = () => {
-        document.getElementById("about").scrollIntoView({behavior: "smooth"})
-    }
+        document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+    };
 
     return (
         <>
-        <section className="py-10 h-screen flex" id="home">
-            <div className="container mx-auto flex justify-center items-center px-5">
-                <div className="grid grid-cols-1 sm:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_3fr_4fr] gap-6 items-center">
-                    <div className="flex sm:flex-row lg:flex-col space-x-4 sm:space-x-4 lg:space-x-0 lg:space-y-4 justify-center sm:justify-start">
-                        <a href="https://www.linkedin.com/in/sohaib-aslam-dev/" className="hover:text-[#6c55e1] text-xl" target="_blank" rel="noopener noreferrer"><FiLinkedin /></a>
-                        <a href="#" className="hover:text-[#6c55e1] text-xl" target="_blank" rel="noopener noreferrer"><TfiWorld /></a>
-                        <a href="https://github.com/Seboo-Dogar" className="hover:text-[#6c55e1] text-xl" target="_blank" rel="noopener noreferrer"><FiGithub /></a>
-                    </div>
+            <section className="py-10 h-screen flex" id="home">
+                <div className="container mx-auto flex justify-center items-center px-10 lg:px-5">
+                    <div className="grid grid-cols-1 md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_3fr_4fr] gap-0 md:gap-6 items-center sm:py-20">
+                        {/* Mobile-first: Image first */}
+                        <div className="flex justify-center order-1 md:order-2 lg:order-3 pt-28 sm:pt-0">
+                            <img
+                                src="./src/assets/Seboo.png"
+                                alt="Seboo Profile"
+                                className="w-60 h-60 lg:w-80 lg:h-80 object-cover"
+                            />
+                        </div>
 
-                    <div className="space-y-4 text-center lg:text-left pt-20">
-                        <h1 className="text-4xl md:text-5xl font-bold">Hi, I'm Sohaib</h1>
-                        <h3 className="text-2xl md:text-3xl font-semibold">Frontend Developer</h3>
-                        <p className="text-base leading-relaxed">I specialize in crafting dynamic and responsive web applications, leveraging modern technologies to deliver high-quality, user-friendly designs and seamless functionality.</p>
-                        <a href="./src/assets/MyCV.pdf" download="MyCV.pdf" className="bg-[#6c55e1] text-white py-2 px-4 rounded-full inline-flex items-center hover:bg-[#5742d7] transition">Download CV <FiDownload className="ml-2" /></a>
-                        
-                        <div className="">
-                            <button onClick={mangeScrolling} className="inline-flex items-center mt-40 animate-bounce outline-none">
-                                <CgMouse className="mx-2 text-xl text-[#6c55e1]" /> Scroll Down<TiArrowDown />
-                            </button>
+                        {/* Text part */}
+                        <div className="space-y-2 sm:space-y-4 text-center md:text-left order-2 md:order-1 lg:order-2 md:pt-20">
+                            <h1 className="text-4xl lg:text-5xl font-bold">Hi, I'm Sohaib</h1>
+                            <h3 className="text-xl lg:text-2xl font-semibold">
+                                Frontend Developer
+                            </h3>
+                            <p className="text-base leading-relaxed">
+                                I specialize in crafting dynamic and responsive web applications,
+                                leveraging modern technologies to deliver high-quality,
+                                user-friendly designs and seamless functionality.
+                            </p>
+
+                            {/* Download Button */}
+                            <a
+                                href="./src/assets/MyCV.pdf"
+                                download="MyCV.pdf"
+                                className="bg-[#6c55e1] text-white py-2 px-4  rounded-full inline-flex items-center hover:bg-[#5742d7] transition"
+                            >
+                                Download CV <FiDownload className="ml-2" />
+                            </a>
+
+                            {/* Scroll Down */}
+                            <div className="hidden sm:block">
+                                <button
+                                    onClick={mangeScrolling}
+                                    className="inline-flex items-center lg:mt-40 animate-bounce outline-none"
+                                >
+                                    <CgMouse className="lg:mx-2 text-xl text-[#6c55e1]" /> 
+                                    <span className="hidden lg:block">Scroll Down</span>
+                                    <TiArrowDown />
+                                </button>
+                            </div>
+                        </div>
+
+                        {/* Social Icons */}
+                        <div className="flex justify-center lg:flex-col md:flex-row space-x-4 md:space-x-4 lg:space-x-0 lg:space-y-4 order-3 md:order-2 lg:order-1 my-5 sm:my-0">
+                            <a
+                                href="https://www.linkedin.com/in/sohaib-aslam-dev/"
+                                className="hover:text-[#6c55e1] text-xl"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FiLinkedin />
+                            </a>
+                            <a
+                                href="#"
+                                className="hover:text-[#6c55e1] text-xl"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <TfiWorld />
+                            </a>
+                            <a
+                                href="https://github.com/Seboo-Dogar"
+                                className="hover:text-[#6c55e1] text-xl"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FiGithub />
+                            </a>
                         </div>
                     </div>
-                    <div className="flex justify-center lg:justify-end pt-10">
-                        <img src="./src/assets/Seboo.png" alt="Seboo Profile" className="w-60 h-60 md:w-80 md:h-80 object-cover"/>
-                    </div>
                 </div>
-            </div>
-        </section>        
+            </section>
         </>
     );
-};
+}
